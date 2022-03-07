b604aa055dedcfb958bda2d4439c1fa39f64918a
Anchal <anchalsing1@gmail.com>
2022-03-07 09:42:49 +0000

    dashboard changes

---
+++ src/app/component/dashboard/dashboard.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { dashboard } from 'src/app/Model/dashboard';
+import { DashboardService } from 'src/app/Services/dashboard.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  dUsersList:dashboard[]=[];
+  constructor(private router :Router ,private httpServ: DashboardService) { }
+
+  ngOnInit(): void {
+    this.getDashboardUsersList();
+  }
+
+  public getDashboardUsersList()
+  {
+   return this.httpServ
+   .getDashboardUsersList()
+   .subscribe((response: dashboard[]) =>{
+                           console.log("getting response  ")
+                           this.dUsersList=response;
+                           console.log(this.dUsersList);
+                             }
+             ) 
+  }
+
+}
